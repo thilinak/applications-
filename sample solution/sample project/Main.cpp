2f142adcb2b46d99688f29b407692c4fc263a17e
Thilina Illangasinghe <thilinaillangasinghe@gmail.com>
2016-03-26 23:55:10 +0530

    third sample commit

--- sample solution/sample project/Main.cpp
+++ sample solution/sample project/Main.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "This is a sample project!" << std::endl;
 	std::cout << "Another change to project for a second commit" << std::endl;
+	std::cout << "Another change to project for a third commit" << std::endl;
 
 	return 0;
 }
