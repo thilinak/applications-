7642f5ec4b659f2f834b577f5ab6ac0d7c6b2b61
Thilina Illangasinghe <thilinaillangasinghe@gmail.com>
2016-03-26 23:56:10 +0530

    forth commit

--- sample solution/sample project/Main.cpp
+++ sample solution/sample project/Main.cpp
@@ -5,6 +5,7 @@ int main()
 	std::cout << "This is a sample project!" << std::endl;
 	std::cout << "Another change to project for a second commit" << std::endl;
 	std::cout << "Another change to project for a third commit" << std::endl;
+	std::cout << "Another change to project for a fourth commit" << std::endl;
 
 	return 0;
 }
